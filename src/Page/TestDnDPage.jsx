cebe866fc2cfac5550efcb8b151926450a177e2d
KDH20160140 <bzidol@naver.com>
2023-01-16 02:25:04 +0900

    feat: 드래그앤 드롭 기능 추가 추가, 수정, 삭제 기능 추가

--- src/Page/TestDnDPage.jsx
+++ src/Page/TestDnDPage.jsx
@@ -2,26 +2,14 @@ import React, { useCallback, useRef, useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const ID_1 = "ID_1";
-const ID_2 = "ID_2";
+const ID_SAVED_AREA = "SAVED_AREA";
+const ID_SEARCH_RESULT_AREA = "SEARCH_RESULT_AREA";
+const ID_DELETE_AREA = "DELETE_AREA";
 
 const TestDnDPage = () => {
-    /*
-    정상적인 이동에 해당하는 경우
-
-    1. 조회된 영역에서 저장된 영역으로 이동하는 경우 (저장하기 기능에 해당한다.)
-    2. 저장된 영역에서 제거 영역으로 이동하는 경우 (삭제하기 기능에 해당한다.)
-    3. 저장된 영역에서 저장된 영역으로 이동하는 경우 (+ 순서 변경에 해당한다.)
-    */
     const dragInfo = useRef({ start: null, end: null });
 
-    const onDragStart = useCallback((e) => {
-        dragInfo.current.start = e;
-    }, []);
-
-    const onDragEnd = useCallback((e) => {
-        console.log("onDragEnd: ", e);
-    }, []);
+    const [showDeleteArea, setShowDeleteArea] = useState(false);
 
     const [savedData, setSavedData] = useState([
         {
@@ -60,6 +48,131 @@ const TestDnDPage = () => {
             name: "search_gray",
         },
     ]);
+
+    /*
+    정상적인 이동에 해당하는 경우
+
+    1. 조회된 영역에서 저장된 영역으로 이동하는 경우 (저장하기 기능에 해당한다.)
+    2. 저장된 영역에서 제거 영역으로 이동하는 경우 (삭제하기 기능에 해당한다.)
+    3. 저장된 영역에서 저장된 영역으로 이동하는 경우 (+ 순서 변경에 해당한다.)
+    */
+    // 이동 체크
+    const checkMovement = useCallback((start, end) => {
+        let type = "ERROR";
+        let msg = "";
+
+        // 에러
+        if (start === ID_SEARCH_RESULT_AREA && end === ID_SEARCH_RESULT_AREA) {
+            type = "ERROR";
+            msg = "조회된 데이터의 순서는 변경할 수 없습니다.";
+        } else if (start === ID_SAVED_AREA && end === ID_SEARCH_RESULT_AREA) {
+            type = "ERROR";
+            msg = "이미 저장된 데이터를 조회 영역으로 이동할 수 없습니다.";
+        }
+        // 정상 이동
+        // 1번 경우에 해당함
+        else if (start === ID_SEARCH_RESULT_AREA && end === ID_SAVED_AREA) {
+            type = "SAVE";
+            msg = "데이터가 저장되었습니다.";
+        }
+        // 2번 경우에 해당함
+        else if (start === ID_SAVED_AREA && end === ID_DELETE_AREA) {
+            type = "DELETE";
+            msg = "데이터가 제거되었습니다.";
+        }
+        // 3번 경우에 해당함
+        else if (start === ID_SAVED_AREA && end === ID_SAVED_AREA) {
+            type = "REORDER";
+            msg = "데이터의 순서가 변경되었습니다.";
+        }
+
+        return { type, msg };
+    }, []);
+
+    const onDragStart = useCallback((e) => {
+        // onDragEnd에서 사용하기 위해 현재 정보를 저장한다.
+        dragInfo.current.start = e;
+
+        const {
+            source: { droppableId },
+        } = e;
+
+        // 만약 저장 영역에서 드래그를 하는 경우 제거 영역을 표시해준다.
+        if (droppableId === ID_SAVED_AREA) {
+            setShowDeleteArea(true);
+        }
+    }, []);
+
+    const onDragEnd = useCallback(
+        (e) => {
+            const startID = dragInfo.current.start.source.droppableId;
+            const endID = e.destination.droppableId;
+
+            // 이동 체크
+            const { type, msg } = checkMovement(startID, endID);
+
+            switch (type) {
+                case "ERROR":
+                    console.warn(msg);
+                    break;
+                case "SAVE":
+                    if (savedData.length === 4) {
+                        alert("최대 4개까지 저장 가능합니다.");
+                        break;
+                    }
+                    console.log("저장하기");
+                    console.log("저장 Index: ", e.destination.index);
+                    const target = searchData.find(
+                        (findItem) => findItem.id === e.draggableId
+                    );
+                    const copySavedData1 = [...savedData];
+                    copySavedData1.splice(e.destination.index, 0, target);
+
+                    setSavedData(copySavedData1);
+
+                    setSearchData((prevSearchData) =>
+                        prevSearchData.filter(
+                            (filterItem) => filterItem.id !== e.draggableId
+                        )
+                    );
+                    break;
+
+                case "DELETE":
+                    console.log("제거하기");
+                    setSavedData((prevSavedData) =>
+                        prevSavedData.filter(
+                            (filterItem) => filterItem.id !== e.draggableId
+                        )
+                    );
+                    break;
+
+                case "REORDER":
+                    console.log("순서 변경하기");
+                    const copySavedData2 = [...savedData];
+                    const target2 =
+                        copySavedData2[dragInfo.current.start.source.index];
+
+                    copySavedData2.splice(
+                        dragInfo.current.start.source.index,
+                        1
+                    );
+                    copySavedData2.splice(e.destination.index, 0, target2);
+
+                    console.log("FROM", dragInfo.current.start.source.index);
+                    console.log("TO", e.destination.index);
+                    setSavedData(copySavedData2);
+                    break;
+            }
+            setShowDeleteArea(false);
+
+            console.log("startID: ", startID);
+            console.log("endID: ", endID);
+            console.log("onDragStart: ", dragInfo.current.start);
+            console.log("onDragEnd: ", e);
+        },
+        [checkMovement, savedData, searchData]
+    );
+
     return (
         <TestDnDPageBlock>
             <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
@@ -68,7 +181,7 @@ const TestDnDPage = () => {
 
                 드래드앤 드롭 이벤트가 종료되어도 클릭 이벤트는 발생하지 않는다
                 */}
-                <Droppable droppableId={ID_1}>
+                <Droppable droppableId={ID_SAVED_AREA}>
                     {(provided, snapshot) => {
                         // console.log("provided: ", provided);
                         // console.log("snapshot: ", snapshot);
@@ -105,7 +218,7 @@ const TestDnDPage = () => {
                         );
                     }}
                 </Droppable>
-                <Droppable droppableId={ID_2}>
+                <Droppable droppableId={ID_SEARCH_RESULT_AREA}>
                     {(provided, snapshot) => {
                         // console.log("provided: ", provided);
                         // console.log("snapshot: ", snapshot);
@@ -141,6 +254,20 @@ const TestDnDPage = () => {
                             </div>
                         );
                     }}
+                </Droppable>
+                <Droppable droppableId={ID_DELETE_AREA}>
+                    {(provided, snapshot) => (
+                        <div
+                            className={`deleteArea ${
+                                showDeleteArea ? "deleteArea_active" : ""
+                            }`}
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            <h1>DELETE ITEM</h1>
+                            {provided.placeholder}
+                        </div>
+                    )}
                 </Droppable>
                 {/* <div className="Area2">
                     <div className="box" />
@@ -182,6 +309,40 @@ const TestDnDPageBlock = styled.div`
         color: white;
         justify-content: center;
         align-items: center;
+    }
+
+    .deleteArea {
+        border-top-left-radius: 30px;
+        border-top-right-radius: 30px;
+
+        z-index: 10;
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 50%;
+        height: 200px;
+        background-color: #d759598f;
+
+
+        transition: 0.2s;
+
+        pointer-event: none;
+        // opacity: 1;
+        opacity: 0;
+
+        border
+    }
+
+    .deleteArea h1{
+        position: absolute;
+        top: -80px;
+        color: red;
+        pointer-event: none;
+    }
+
+
+    .deleteArea_active {
+        opacity: 1;
     }
 `;
 export default TestDnDPage;
