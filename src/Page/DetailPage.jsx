1703aec1745866a21377c0500685f7ef42603d91
KDH20160140 <bzidol@naver.com>
2023-01-17 16:46:06 +0900

    feat: 사용자 정보 표시

--- src/Page/DetailPage.jsx
+++ src/Page/DetailPage.jsx
@@ -1,3 +1,4 @@
+import { Divider } from "@mui/material";
 import React from "react";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
@@ -5,7 +6,43 @@ import styled from "styled-components";
 const DetailPage = () => {
     const location = useLocation();
 
-    return <DetailPageBlock>DetailPage</DetailPageBlock>;
+    /*
+    필수
+    선택된 Repo의 issue가 보여야 한다.
+
+    각 issue는 제목, repo 명이 필수로 보여져야 한다
+
+    issue의 내용이 길 수 있으니 높이는 가변으로 한다.
+    */
+
+    console.log("location: ", location);
+    return (
+        <DetailPageBlock>
+            <div className="ownerInfo">
+                {/* 저장소 주인 이미지 */}
+                <div>
+                    <img
+                        src={location.state.owner.avatar_url}
+                        alt="userImage"
+                    />
+                </div>
+                {/* 저장소 주인 이름 */}
+                <div>{location.state.owner.login}</div>
+                {/* 저장소 주인 링크 */}
+                <div>
+                    <a
+                        target={"_blank"}
+                        href={location.state.owner.html_url}
+                        rel="noreferrer"
+                    >
+                        Visit Repository
+                    </a>
+                </div>
+            </div>
+            <Divider orientation="vertical" />
+            <div className="issue"></div>
+        </DetailPageBlock>
+    );
 };
 
 const DetailPageBlock = styled.div`
@@ -14,5 +51,27 @@ const DetailPageBlock = styled.div`
 
     flex: 6;
     background-color: #f6f8fa;
+
+    display: flex;
+
+    gap: 5px;
+
+    .issue,
+    .ownerInfo {
+        height: 100%;
+
+        padding: 10px;
+        box-sizing: border-box;
+    }
+
+    .ownerInfo {
+        flex: 1;
+        // border: 1px solid lightgreen;
+    }
+
+    .issue {
+        flex: 3;
+        // border: 1px solid gray;
+    }
 `;
 export default DetailPage;
