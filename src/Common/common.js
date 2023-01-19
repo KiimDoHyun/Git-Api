a0d6ad0450865920bc070ae0b98661d684643378
KDH20160140 <bzidol@naver.com>
2023-01-19 21:44:49 +0900

    feat: 배경색에 따른 글자색 조정 방식 수정 배경색 판단 함수생성(웹 코드 참고)

--- src/Common/common.js
+++ src/Common/common.js
@@ -23,3 +23,38 @@ export const getLocalStorage = (localStorageName = "") => {
 
     return JSON.parse(window.localStorage.getItem(localStorageName));
 };
+
+// 배경색에 따른 글자색 조정
+// 어두운 배경 -> 흰색
+// 밝은 배경 -> 검은색
+// 출처: https://gist.github.com/krabs-github/ec56e4f1c12cddf86ae9c551aa9d9e04
+export const defineLightOrDark = (backgroundColor) => {
+    let r = null;
+    let g = null;
+    let b = null;
+    let hsp = null;
+
+    let color = +(
+        "0x" +
+        backgroundColor
+            .slice(1)
+            .replace(backgroundColor.length < 5 && /./g, "$&$&")
+    );
+
+    r = color >> 16;
+    g = (color >> 8) & 255;
+    b = color & 255;
+
+    // HSP equation from http://alienryderflex.com/hsp.html
+    hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
+
+    // Using the HSP value, determine whether the color is light or dark
+    // light
+    if (hsp > 127.5) {
+        return "black";
+    }
+    // dark
+    else {
+        return "white";
+    }
+};
--- src/Component/Detail/IssueListItem.jsx
+++ src/Component/Detail/IssueListItem.jsx
@@ -1,23 +1,6 @@
 import { Card, CardContent, Chip, Tooltip, Typography } from "@mui/material";
 import React from "react";
-
-// 하얀 글자를 가지는 배경색
-const whiteColorList = [
-    "000000",
-    "0025ff",
-    "0366d6",
-    "cc317c",
-    "b60205",
-    "9149d1",
-    "473bcc",
-];
-
-// 배경색에 따라 글자 색을 바꾼다.
-const findColor = (inputColor) => {
-    return whiteColorList.find((findItem) => findItem === inputColor)
-        ? "white"
-        : "black";
-};
+import { defineLightOrDark } from "../../Common/common";
 
 const IssueListItem = ({
     title = "클릭하면 해당 Issue 페이지로 이동합니다.",
@@ -44,19 +27,16 @@ const IssueListItem = ({
 
                     {/* 이슈 라벨 */}
                     <div className="issueLabel">
-                        {labels.map((item, idx) => {
-                            const color = findColor(item.color);
-                            return (
-                                <Chip
-                                    key={idx}
-                                    label={item.name}
-                                    style={{
-                                        backgroundColor: `#${item.color}`,
-                                        color,
-                                    }}
-                                />
-                            );
-                        })}
+                        {labels.map((item, idx) => (
+                            <Chip
+                                key={idx}
+                                label={item.name}
+                                style={{
+                                    backgroundColor: `#${item.color}`,
+                                    color: defineLightOrDark(item.color),
+                                }}
+                            />
+                        ))}
                     </div>
 
                     {/* 이슈 생성자 */}
